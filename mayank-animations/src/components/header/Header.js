d2cf72b12a0d5b34981ca489456f8be75665941f
Mayank <mayankverma76342@gmail.com>
2020-09-12 02:20:01 -0700

    initialise the app
    initialise the app

---
+++ mayank-animations/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header"
+function Header() {
+    return (
+        <div className="Header">
+            {/* https://ryancv.bslthemes.com/ve1/#home-e */}
+        </div>
+    )
+}
+
+export default Header
